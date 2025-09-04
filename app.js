8a96fc67ede3cd8381b524269e7ed94b7b1ab3b6
Nithin Dasari <nithindasari10@gmail.com>
2025-09-04 19:33:39 +0530

    Edit,New,Index and show route styling done

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/skyward";
+
+delete mongoose.connection.models["Listing"];
 
 main()
   .then(() => {
